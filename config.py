68be8830127823b9dd1ff32142d9249cf9e371ed
calebkish <calalk@att.net>
2019-11-09 02:25:12 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,31 @@
+# NOTICE: Chromedriver is requred for this to work.
+# Download: https://chromedriver.chromium.org/
+# Make sure to download the version that corresponds to your installed Chrome 
+# browser version
+
+
+# TEMPLATE:
+
+# The 'link' can be found by right-clicking the Wistia embeded video player and 
+# clicking "Copy link and thumbnail"
+#
+# The 'path' must have an ending "/" (indicates that it is a folder)
+#
+# The 'filename' will have ".mp4" appended onto it later
+videos = [
+    # {
+    #     'link': '<p><a href="https://www.website.com?wvideo=kqzjaftn6u"><img src="https://embedwistia-a.akamaihd.net/deliveries/b1d23ad43833532b734f11b8c18f8f72.jpg?image_play_button_size=2x&amp;image_crop_resized=960x540&amp;image_play_button=1&amp;image_play_button_color=54bbffe0" style="width: 400px; height: 225px;" width="400" height="225"></a></p><p><a href="https://www.website.com?wvideo=kqzjaphn6u">Video title</a></p>',
+    #     'path': 'path/to/file/',
+    #     'filename': 'really-cool-file-name'
+    # }
+]
+
+# 720 is the desired quality, and if it doesn't exist then a subsequent quality 
+# will be chosen
+qualities = [
+    '720',
+    '540',
+    '1080',
+    '360',
+    '224'
+]
---
+++ wistia.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from dotenv import load_dotenv
+import os
+from os.path import join, dirname
+import urllib.request
+import time
+import config
+
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+
+driver = webdriver.Chrome()
+
+for video in config.videos:
+    id = video['link'].split('"')[1].split('=')[1]
+
+    driver.get("https://getvideo.at/en/")
+    
+    driver.find_element(By.ID, "search-text").send_keys("https://fast.wistia.net/embed/iframe/" + id)
+
+    driver.find_element(By.ID, "search-button").click()
+
+    time.sleep(3)
+    
+    for quality in config.qualities:
+        test_xpath = "//*[contains(text(),'" + quality + "')]/parent::*"
+        
+        if check_exists_by_xpath(test_xpath):
+            video_url = driver.find_element(By.XPATH, test_xpath).get_attribute("href")
+            break
+
+    if not os.path.exists(video['path']):
+        os.makedirs(video['path'])
+
+    urllib.request.urlretrieve(video_url, video['path'] + video['filename'] + '.mp4')
+
